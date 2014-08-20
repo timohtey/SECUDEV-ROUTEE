511e0629e042af3a67dca5ec967b07812ae9c67b
Timothy Rodriguez <timohtey.rodriguez136@gmail.com>
2014-08-20 12:59:47 +0800

    Initial Commit

---
+++ go.php
@@ -0,0 +1,29 @@
+<?php
+
+$db_username = 'root';
+$db_password = '';
+$db_name = 'routee';
+$db_host = 'localhost';
+
+$con= mysqli_connect($db_host, $db_username, $db_password, $db_name);
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+// escape variables for security
+$username = mysqli_real_escape_string($con, $_POST['regUSer']);
+$password = mysqli_real_escape_string($con, $_POST['regPass']);
+$email = mysqli_real_escape_string($con, $_POST['regMail']);
+
+$sql="INSERT INTO member (username, password, email
+VALUES ('$username', '$password', '$email')";
+
+if (!mysqli_query($con,$sql)) {
+  die('Error: ' . mysqli_error($con));
+}
+echo "1 record added";
+
+mysqli_close($con);
+
+?>
