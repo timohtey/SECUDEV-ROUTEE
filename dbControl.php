99898decca21da8df378d4458345929f0ff96702
Timothy Rodriguez <timohtey.rodriguez136@gmail.com>
2014-08-22 10:24:05 +0800

    Saved reported_by.

--- dbControl.php
+++ dbControl.php
@@ -1,5 +1,10 @@
 <?php
 
+start_session();
+if(isset($_SESSION['username'])){
+    $username = $_SESSION['username'];
+}
+
 // database settings 
 $db_username = 'root';
 $db_password = '';
@@ -40,7 +45,7 @@ if ($_POST) {
     $mDate = filter_var($_POST["date"], FILTER_SANITIZE_STRING);
     $mDeleted = filter_var($_POST["deleted"], FILTER_SANITIZE_STRING);
 
-    $results = $mysqli->query("INSERT INTO markers (description, lat, lng, type, address, date, deleted) VALUES ('$mDesc',$mLat, $mLng, '$mType', '$mAddress', '$mDate', '$mDeleted')");
+    $results = $mysqli->query("INSERT INTO markers (description, lat, lng, type, address, date, deleted, resolved_by) VALUES ('$mDesc',$mLat, $mLng, '$mType', '$mAddress', '$mDate', '$mDeleted', '$username')");
     if (!$results) {
         header('HTTP/1.1 500 Error: Could not create marker!');
         exit();
