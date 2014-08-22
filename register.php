53d5f6b8341e3ca5cd0a90d81ae7242386b51b49
JovenCruz <jovencruz23@gmail.com>
2014-08-22 10:19:03 +0800

    Lego House

--- register.php
+++ register.php
@@ -86,7 +86,7 @@
                         return false;
                     }
                 };
-            }
+            
 
             // function validateEmail() {
             //     var email = $(document).getElementById('regMailText');
@@ -97,6 +97,7 @@
             window.onload = function() {
                 RegistrationErrorHandlers();
             };
+            
     </script>
 
     </head>
